4cc96bac2f62b3b05704ad5f859c09447d70fe01
Randy Coulman <rcoulman@gmail.com>
2015-04-28 19:15:30 -0700

    Add spec for $0.00

--- coin_changer_spec.rb
+++ coin_changer_spec.rb
@@ -2,12 +2,12 @@ require 'rspec'
 require_relative 'coin_changer'
 
 RSpec.describe CoinChanger do
+  subject(:coin_changer) { CoinChanger.new }
+
   describe 'acceptance' do
     it 'returns a combination of coins' do
       pending 'Not ready to pass acceptance test yet'
-      coin_changer = CoinChanger.new
-      amount = 394
-      change = coin_changer.change_for(amount)
+      change = coin_changer.change_for(394)
       coins = {
         100 => 3,
         50 => 1,
@@ -17,6 +17,13 @@ RSpec.describe CoinChanger do
         1 => 4
       }
       expect(change).to eq coins
+    end
+  end
+
+  describe 'unit specs' do
+    it 'returns no coins for $0.00' do
+      change = coin_changer.change_for(0)
+      expect(change).to eq({})
     end
   end
 end
